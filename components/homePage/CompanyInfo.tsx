e569d7dc7fbf908845e0c0c533df16d90cf0924c
m-aljamal <mohammadjamol@gmail.com>
2021-06-15 08:42:23 +0300

    start company

--- components/homePage/CompanyInfo.tsx
+++ components/homePage/CompanyInfo.tsx
@@ -3,20 +3,21 @@ import { companyInfSection } from "../../utils/homePageText";
 
 export default function CompanyInfo() {
   return (
-    <section className="bg-office w-full   ">
-      <div className="flex bg-transparentWhite">
-        <div className="bg-red-500 w-1/2 py-20">
-          <div className="container">
-            <h2>{companyInfSection.title}</h2>
-            <p>{companyInfSection.text}</p>
-            <p>{companyInfSection.text2}</p>
-            {companyInfSection?.numbers?.map((n) => (
-              <Numbers number={n.number} text={n.text} />
-            ))}
-          </div>
+    <section className="relative bg-yellow">
+      <div className="flex">
+        <div className="w-1/2 absolute top-0 left-0 h-full p-0  imgBorder z-10 flex">
+          <figure className="bg-office bg-cover bg-right-top top-0 left-0 block w-full h-full"></figure>
         </div>
-        <div className=" w-1/2 py-20  ">
-          <div className="w-full bg-none">ff</div>
+        <div className="w-1/2">
+          <h2>fdfdfd</h2>
+          <h2>fdfdfd</h2>
+          <h2>fdfdfd</h2>
+          <h2>fdfdfd</h2>
+          <h2>fdfdfd</h2>
+          <h2>fdfdfd</h2>
+          <h2>fdfdfd</h2>
+          <h2>fdfdfd</h2>
+          <h2>fdfdfd</h2>
         </div>
       </div>
     </section>
@@ -31,3 +32,24 @@ const Numbers = ({ number, text }) => {
     </div>
   );
 };
+
+{
+  /* <section className="bg-office w-full elative ">
+      <div className="flex ">
+        <div className="bg-red-500 w-1/2 py-20">
+          <div className="container">
+            <h2>{companyInfSection.title}</h2>
+            <p>{companyInfSection.text}</p>
+            <p>{companyInfSection.text2}</p>
+            {companyInfSection?.numbers?.map((n) => (
+              <Numbers number={n.number} text={n.text} />
+            ))}
+          </div>
+        </div>
+        <div className=" w-1/2 py-20  ">
+          <div className="w-full"></div>
+        </div>
+      </div>
+      
+    </section> */
+}
